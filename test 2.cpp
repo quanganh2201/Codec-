2d28bc4786c75a4ea81d3aa9fe992646ce6976cc
quanganh2201 <100138950+quanganh2201@users.noreply.github.com>
2022-02-21 22:58:45 +0700

    Add files via upload

---
+++ test 2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+ void sort(float array[], int size, bool isAscending){
+	 if (isAscending==true){
+ 		for (int i=0; i< size;i++){
+ 			for(int j=i+1; j<size;j++){
+ 				if (array[i]>array[j]){
+ 					int doi= array[i];
+ 					array[i]=array[j];
+ 					array[j]=doi;
+				 }
+			 }
+		 }
+	 }
+	 else {
+	 	for (int i=0; i<size;i++){
+ 			for(int j=i+1; j<size;j++){
+ 				if (array[i]<array[j]){
+ 					int doi= array[i];
+ 					array[i]=array[j];
+ 					array[j]=doi;
+ 				}
+ 			}
+ 		}
+	}
+
+ }
+ int main(){
+ 	float array[1000];
+ 	int size;
+ 	bool isAscending;
+ 	cin >> size;
+ 	for (int i=0; i<size;i++){
+ 		cin >> array[i];
+	 }
+	cin >>isAscending;
+	sort (array,size,isAscending);
+	for (int i=0; i< size; i++){
+		cout << array[i]<<"; ";
+	}
+	return 0;
+}
+ 
